971228ebea4eba31b782969e6d0a377b80bdb765
adam <adamadalui@gmail.com>
2025-09-28 15:13:33 +0100

    Modification du code

--- src/comperson/Enseignant.java
+++ src/comperson/Enseignant.java
@@ -1,5 +1,35 @@
 package comperson;
 
-public class Enseignant {
 
+public class Enseignant extends Personnel {
+
+ private String matiereEnseignant;
+
+
+ public Enseignant() {
+ }
+
+ public Enseignant(String nom, byte age, String adresse, float salaire, String matiereEnseignant) {
+     super(nom, age, adresse, salaire);
+     this.matiereEnseignant = matiereEnseignant;
+ }
+
+ public Enseignant(Enseignant e) {
+     super(e.getNom(), e.getAge(), e.getAdresse(), e.getSalaire());
+     this.matiereEnseignant = e.matiereEnseignant;
+ }
+
+
+ public String getMatiereEnseignant() {
+     return matiereEnseignant;
+ }
+
+ public void setMatiereEnseignant(String matiereEnseignant) {
+     this.matiereEnseignant = matiereEnseignant;
+ }
+
+ public String toString() {
+     return super.toString() + ", Matière : " + matiereEnseignant;
+ }
 }
+
